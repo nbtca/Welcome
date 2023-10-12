7eac009c341e8893119833ecee3cbe65089836ed
LazuliKao <LazuliKao233@outlook.com>
2023-10-12 16:52:05 +0800

    fix route

--- src/src/components/AutoNavLink.vue
+++ src/src/components/AutoNavLink.vue
@@ -1,34 +1,63 @@
 <template>
   <ion-nav-link
+    :class="{
+      active: active,
+    }"
     ref="nav"
-    data-a="测试"
     :component="component"
     :component-props="componentProps"
   >
     <slot></slot>
   </ion-nav-link>
 </template>
-<style scoped></style>
+<style scoped>
+.active::after {
+  --border-width: 10px;
+  content: "";
+  position: fixed;
+  top: 0px;
+  width: calc(100% - 2 * var(--border-width));
+  height: calc(100% - 2 * var(--border-width));
+  /* background-color: var(--ion-color-primary); */
+  animation: active 1s ease-in-out;
+  border: var(--border-width) solid var(--ion-color-primary);
+}
+@keyframes active {
+  0% {
+    transform: scale(0);
+    opacity: 0;
+  }
+  50% {
+    transform: scale(1.2);
+    opacity: 0.5;
+  }
+  100% {
+    transform: scale(1);
+    opacity: 0;
+  }
+}
+</style>
 <script lang="ts" setup>
 import router from "@/router";
-
+import { GetPageNameFromComponment, locationPath } from "@/router/heper";
+const active = ref(false);
 const props = defineProps<{
   component?: Object;
   componentProps?: Object;
 }>();
 const component = props.component as HTMLElement & { __name: string };
 const nav = ref<HTMLIonNavLinkElement & { $el: HTMLIonNavLinkElement }>();
-
-const componentName = component.__name;
-console.log(componentName);
 if (
-  router.currentRoute.value.query.location == componentName.toLocaleLowerCase()
+  router.currentRoute.value.query[locationPath] ==
+  GetPageNameFromComponment(component)
 ) {
+  active.value = true;
   setTimeout(() => {
     const el = nav.value?.$el;
     if (el) {
       el.click();
     }
-  }, 250);
+    active.value = false;
+  }, 1250);
 }
 </script>
--- src/src/router/heper.ts
+++ src/src/router/heper.ts
@@ -1,17 +1,29 @@
 import router from ".";
-
+export const locationPath = "location";
+export function GetPageNameFromComponment(component: any) {
+  const name = component.__name.toLowerCase();
+  if (name === "homepage") return undefined;
+  //trimend 'page'
+  if (name.endsWith("page")) return name.substring(0, name.length - 4);
+  return name;
+}
 export async function ionNavDidChange(el: HTMLIonNavElement) {
   const active = await el.getActive();
   if (!active) return;
-  const file = (active.component.__name as string).toLowerCase();
+  const file = GetPageNameFromComponment(active.component);
+  const originalQuery = router.currentRoute.value.query;
   if (file === "homepage") {
     router.push({
-      query: {},
+      query: {
+        [locationPath]: undefined,
+        ...originalQuery,
+      },
     });
   } else {
     router.push({
       query: {
-        location: file,
+        [locationPath]: file,
+        ...originalQuery,
       },
     });
   }
